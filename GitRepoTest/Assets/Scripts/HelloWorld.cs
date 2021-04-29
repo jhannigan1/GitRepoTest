1fab32e7f39f6e1d43593a6780ccd33aa9c2edd8
jhannigan1 <jhannigan1@mail.csuchico.edu>
2021-04-29 13:24:53 -0700

    GitHub Repo Test
    Pushing initial commit with 2 scripts to test repo functionality.

---
+++ GitRepoTest/Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Hello Git.");
+    }
+}
